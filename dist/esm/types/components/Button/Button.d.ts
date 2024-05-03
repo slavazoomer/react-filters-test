eae2f530fc947bdab71242c6dc4133c47b595b77
slavazoomer <136600271+slavazoomer@users.noreply.github.com>
2024-05-03 19:37:24 +0300

    Delete dist directory

--- dist/esm/types/components/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import './Button.css';
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: 'primary' | 'secondary';
-}
-declare const Button: React.FC<ButtonProps>;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: 'primary' | 'secondary';
-}
-declare const Button: React.FC<ButtonProps>;
-
-export { Button };
